1901adf736fd79b95b155098df91d11edecb51ec
steve <sliriano2020@gmail.com>
2019-09-15 17:43:14 -0400

    finalize crypto db table functionality, deprecate vanilla javascript api functionality

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php
+    // connect to database
+    $conn = mysqli_connect('localhost', 'steve', 'INSERT PASSWORD HERE', 'market_data');
+
+    // check connection
+    if (!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    // query for getting the top 100 coins in db
+    $sql = 'SELECT * FROM crypto ORDER BY marketcap';
+    $result = mysqli_query($conn, $sql);
+
+    // feth the resulting rows as an array, reverse the array to descending order
+    $coins = array_reverse(mysqli_fetch_all($result, MYSQLI_ASSOC));
+    
+    // free result from memory
+    mysqli_free_result($result);
+    //close connection to database
+    mysqli_close($conn);
+    
+    $rank =0;
+    function percent_color($num) {
+        if ($num < 0) {
+            return "#e15241";
+        }
+        else if ($num > 0) {
+            return "#43aa05";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html>
    <head>
@@ -6,7 +37,7 @@
       <script type="text/javascript" src="/mktcap/cryptoData.js"></script>      
    </head>
    
-   <body onload="fillCryptoData()">
+   <body >
    <h1 style="text-align: center">MktCap</h1>
     <table id="table" align="center">
         <tr>
@@ -18,7 +49,28 @@
             <th>Circulating Supply</th>
             <th>Change (24)</th>
         </tr>
-
+        <!------Loops through top 100 coins in database and inserts them into html table------->
+        <?php foreach($coins as $coin){ ?>
+            <?php 
+                if ($rank === 100) {
+                    break;
+                }
+            ?>
+            <?php $rank+=1;?>
+            <tr>
+                <td><?php echo $coin['rank']?></td>
+                <td>
+                <img height=18 width=18 src=<?php echo $coin['image_url']?>/>
+                <?php echo htmlspecialchars($coin['cryptocurrency_name'])?> 
+                <?php echo " "."(".htmlspecialchars($coin['ticker']).")"?>
+                </td>
+                <td><?php echo '$'.number_format($coin['marketcap'], 2, '.', ',')?></td>
+                <td style='color: #1070e0;'><?php echo '$'.number_format($coin['price'], 2, '.', ',')?></td>
+                <td style='color: #1070e0;'><?php echo '$'.number_format($coin['volume'], 2, '.', ',')?></td>
+                <td><?php echo number_format($coin['supply'],0, '.', ',')?></td>
+                <td style='color: <?php echo percent_color($coin['change24h'])?>'><?php echo $coin['change24h'].'%'?></td>
+            </tr>
+        <?php } ?>
     </table>
    
    </body>
--- scripts/crypto_table_db_update.php
+++ scripts/crypto_table_db_update.php
@@ -7,5 +7,28 @@ if (!$conn) {
     echo 'Connection error: ' . mysqli_connect_error();
 }
 
+for ($i = 0; $i < 10; $i++) {
+    $api_url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page='.strval($i+1).'&sparkline=true&price_change_percentage=24h';
+    $crypto_data = file_get_contents($api_url);
+    $crypto_data = json_decode($crypto_data, true);
+    for ($coin_index = 0; $coin_index < count($crypto_data); $coin_index++) {
+        $name = $crypto_data[$coin_index]['name'];
+        $ticker = strtoupper($crypto_data[$coin_index]['symbol']);
+        $marketcap = $crypto_data[$coin_index]['market_cap'];
+        $price = $crypto_data[$coin_index]['current_price'];
+        $volume = $crypto_data[$coin_index]['total_volume'];
+        $supply = $crypto_data[$coin_index]['circulating_supply'];
+        $change24h = $crypto_data[$coin_index]['price_change_percentage_24h'];
+        $url = $crypto_data[$coin_index]['image'];
+        // attempt to insert data to database
+        $sql = "REPLACE INTO crypto (cryptocurrency_name, ticker, marketcap, price, volume, supply, change24h, image_url) VALUES ('$name', '$ticker', $marketcap, $price, $volume, $supply, $change24h, '$url')";
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }   
+    }
+}
+mysqli_close($conn);
 
 ?>
